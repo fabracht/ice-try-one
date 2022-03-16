10a3c1b4d491f47d2a23eba1acc720ee2c788f77
Fabricio Bracht <f.bracht@ai-systems.ca>
2022-03-15 19:34:05 -0700

    First commit

---
+++ src/main.rs
@@ -0,0 +1,99 @@
+#![allow(unused)]
+use std::{future::Future, pin::Pin, sync::Arc, time::Duration};
+mod tcp_listener;
+use tcp_listener::TcpServer;
+use tokio::{net::UdpSocket, sync::mpsc};
+use webrtc_ice::{
+    agent::{agent_config::AgentConfig, Agent},
+    candidate::Candidate,
+    network_type::NetworkType,
+    state::ConnectionState,
+    udp_mux::{UDPMuxDefault, UDPMuxParams},
+    udp_network::UDPNetwork,
+    Error,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // let is_controlling = false;
+    // let use_mux = false;
+
+    // let remote_port = 4000;
+
+    // let ice_agent = create_ice_agent(is_controlling).await.unwrap();
+    // ice_agent.on_candidate(handle_candidate()).await;
+    // let (ice_done_tx, mut ice_done_rx) = mpsc::channel::<()>(1);
+
+    // ice_agent
+    //     .on_connection_state_change(Box::new(move |c: ConnectionState| {
+    //         println!("ICE Connection State has changed: {}", c);
+    //         if c == ConnectionState::Failed {
+    //             let _ = ice_done_tx.try_send(());
+    //         }
+    //         Box::pin(async move {})
+    //     }))
+    //     .await;
+    // // Get the local auth details and send to remote peer
+    // let (local_ufrag, local_pwd) = ice_agent.get_local_user_credentials().await;
+
+    // println!("posting remoteAuth with {}:{}", local_ufrag, local_pwd);
+    // ice_agent.gather_candidates().await?;
+    // println!("Connecting...");
+    // let (_cancel_tx, cancel_rx) = mpsc::channel(1);
+    //     let (remote_ufrag, remote_pwd) = (local_ufrag,local_pwd);
+    // let conn = ice_agent.dial(cancel_rx, remote_ufrag, remote_pwd).await?;
+    // let weak_conn = Arc::downgrade(&conn);
+    
+    // let mut int = tokio::time::interval(Duration::from_secs(1));
+    // let weak_agent = Arc::downgrade(&ice_agent);
+    // loop {
+    //     int.tick().await;
+    //     // println!(
+    //     //     "weak_agent: weak count = {}, strong count = {}",
+    //     //     weak_agent.weak_count(),
+    //     //     weak_agent.strong_count(),
+    //     // );
+    //     if weak_agent.strong_count() == 0 {
+    //         break;
+    //     }
+    // }
+
+    let server =     TcpServer::new(8080, "localhost");
+    let _ = server.connect().await;
+    let mut input = String::new();
+
+    std::io::stdin().read_line(&mut input)?;
+    Ok(())
+}
+
+async fn create_ice_agent(is_controlling: bool) -> Result<Arc<Agent>, Error> {
+    let local_port = if is_controlling { 4000 } else { 4001};
+
+    let udp_network = {
+        let udp_socket = UdpSocket::bind(("0.0.0.0", local_port)).await?;
+        let udp_mux = UDPMuxDefault::new(UDPMuxParams::new(udp_socket));
+        UDPNetwork::Muxed(udp_mux)
+    };
+    let agent = Agent::new(AgentConfig {
+        network_types: vec![NetworkType::Udp4],
+        udp_network,
+        ..Default::default()
+    })
+    .await?;
+    let ice_agent = Arc::new(agent);
+    Ok(ice_agent)
+}
+
+fn handle_candidate<'a>() -> Box<
+    dyn FnMut(Option<Arc<dyn Candidate + Send + Sync>>) -> Pin<Box<dyn Future<Output = ()> + Send>>
+        + Send
+        + Sync,
+> {
+    Box::new(move |c: Option<Arc<dyn Candidate + Send + Sync>>| {
+        Box::pin(async move {
+            if let Some(c) = c {
+                println!("posting remoteCandidate with {}", c.marshal());
+            }
+        })
+    })
+}
---
+++ src/tcp_listener.rs
@@ -0,0 +1,123 @@
+use std::error::Error;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+
+use dashmap::DashMap;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpSocket, TcpStream};
+use tokio::net::tcp::OwnedWriteHalf;
+// use tokio_util::codec::{Framed, LinesCodec};
+use uuid::Uuid;
+use tokio_codec::{FramedRead, LinesCodec};
+use tokio_util::codec::Framed;
+
+type TcpConnections = Arc<DashMap<Uuid, OwnedWriteHalf>>;
+
+pub struct TcpServer {
+    port: u16,
+    address: &'static str,
+    connections: TcpConnections,
+}
+
+impl TcpServer {
+    pub fn new(port: u16, address: &'static str) -> Self {
+        TcpServer { port, address, connections: Arc::new(DashMap::new()) }
+    }
+
+    pub async fn connect(&self) -> Result<(), tokio::io::Error> {
+        let addr = "127.0.0.1:9001".parse().unwrap();
+        let socket = TcpSocket::new_v4()?;
+        socket.set_reuseaddr(true);
+        socket.set_reuseport(true);
+        socket.bind(addr)?;
+
+        let listener = socket.listen(128)?;
+        let clients = Arc::clone(&self.connections);
+
+        tokio::spawn(async move {
+            connection_handler(clients, listener).await;
+        });
+        println!("Here");
+        self.on_start().await;
+        Ok(())
+    }
+
+    async fn on_start(&self) {
+        let mut now = Instant::now();
+        loop {
+
+            let instant_now = Instant::now();
+            if instant_now.duration_since(now) > Duration::from_millis(2000) {
+                for mut connection in self.connections.iter_mut() {
+                    connection.writable().await;
+                    let mut client = connection.try_write(b"").unwrap();
+                }
+                now = instant_now;
+            }
+        }
+    }
+}
+
+
+async fn match_listener(map: TcpConnections, stream: TcpStream, socket_address: SocketAddr) -> Result<(), Box<dyn Error>> {
+    println!("Someone connected");
+    let id = Uuid::new_v4();
+    let (mut stream, mut sink) = stream.into_split();
+    // let mut lines: Framed<TcpStream, LinesCodec> = Framed::new(stream, LinesCodec::new());
+    // lines.get_ref().try_write(b"Message");
+    // lines.send("Hi there!").await.unwrap();
+    map.entry(id).or_insert(sink);
+
+
+    tokio::spawn(async move {
+        let map = Arc::clone(&map);
+        loop {
+            let mut buf = [0; 4096];
+            // let tcp = lines.get_ref();
+            stream.readable().await;
+            match stream.try_read(&mut buf) {
+                Ok(0) => break,
+                Ok(n) => {
+                    let message = std::str::from_utf8(&buf[..n]).unwrap();
+                    for connection in map.iter_mut() {
+                        if *connection.key() != id.clone() {
+                            connection.writable().await;
+                            let mut client = connection.try_write(&buf[..n]).unwrap();
+                        }
+
+                    }
+                    println!("read {}", message);
+                }
+                Err(ref e) if e.kind() == tokio::io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    eprintln!("{:?}", e);
+                }
+            };
+        }
+
+    });
+
+    Ok(())
+}
+
+async fn connection_handler(map: TcpConnections, listener: TcpListener) {
+    loop {
+        tokio::select! {
+                result = listener.accept() => {
+                    match result {
+                        Ok((stream, addr)) => {
+                            println!("Adding tcpstream 1");
+                            let clients = Arc::clone(&map);
+                            match_listener(clients, stream, addr).await;
+                        },
+                        Err(e) => {
+                            eprintln!("{:?}", e);
+                        }
+                    }
+                }
+            }
+    }
+}
